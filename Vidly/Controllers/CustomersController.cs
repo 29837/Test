d9fbfbebc6aa0a5c8ee24ecf2e0605c83281a58a
Fernando Toledo Ramirez <fernando_29837@live.com>
2017-04-07 20:01:52 -0500

    First MVC Test - Udemy

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name  = "John Smith", Id = 0},
+                new Customer { Name  = "Mary Williams", Id = 1}
+            };
+            var viewModel = new CustomersViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ Vidly/Controllers/DetailsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class DetailsController : Controller
+    {
+        // GET: Details
+        // GET: Customers/Details/id
+        [Route("Customer/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name  = "John Smith", Id = 0},
+                new Customer { Name  = "Mary Williams", Id = 1}
+            };
+            var customer = customers.ElementAt(id);
+            return View(customer);
+        }
+    }
+}
